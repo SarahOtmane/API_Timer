00d8b2a1d334d4d29c6c8db87640e50ca1f80816
SarahOtmane <sarahotmane02@gmail.com>
2024-10-08 14:49:53 +0200

    def of the routes

---
+++ api/src/services/defRoutes.ts
@@ -0,0 +1,16 @@
+import express, { Application } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import configSwagger from '../config/swagger-config';
+
+const configureServices = (app: Application): void => {
+  // Swagger documentation route
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(configSwagger));
+
+  // Middleware for parsing requests
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // Define application routes here
+};
+
+export default configureServices;
