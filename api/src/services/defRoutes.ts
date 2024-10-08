1bde7bcffe10385574ccf72232a821f648c65ceb
SarahOtmane <sarahotmane02@gmail.com>
2024-10-08 16:36:33 +0200

    import de la route du user

--- api/src/services/defRoutes.ts
+++ api/src/services/defRoutes.ts
@@ -2,6 +2,8 @@ import express, { Application } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import configSwagger from '../config/swagger-config';
 
+import userRoutes from '../routes/userRoute'; 
+
 const configureServices = (app: Application): void => {
   // Swagger documentation route
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(configSwagger));
@@ -11,6 +13,7 @@ const configureServices = (app: Application): void => {
   app.use(express.json());
 
   // Define application routes here
+  app.use('/users', userRoutes);
 };
 
 export default configureServices;
