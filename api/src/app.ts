04d1faf054bf879a71933dbd05195d81ab5df7de
SarahOtmane <sarahotmane02@gmail.com>
2024-10-08 14:57:43 +0200

    start serveur

---
+++ api/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application } from 'express';
+
+import connectDB from './services/connectDB';
+import startServeur from './services/serveur';
+import configureServices from './services/defRoutes';
+
+
+const app: Application = express();
+
+connectDB();
+configureServices(app);
+startServeur(app);
+
