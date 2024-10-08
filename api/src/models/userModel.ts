f327166540f716e51570b9729b7848b22cbb08eb
SarahOtmane <sarahotmane02@gmail.com>
2024-10-08 14:59:54 +0200

    model du user

---
+++ api/src/models/userModel.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface IUser extends Document {
+    email: string;
+    password: string;
+    role: boolean;
+}
+
+const userSchema: Schema = new Schema<IUser>({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+const UserModel = mongoose.model<IUser>('User', userSchema);
+
+export default UserModel;
