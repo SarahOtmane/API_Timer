deb0f40cd119ecdf873ed09232f9092a6895c923
SarahOtmane <sarahotmane02@gmail.com>
2024-10-09 09:59:50 +0200

    update user swagger

--- api/src/docs/userSwagger.ts
+++ api/src/docs/userSwagger.ts
@@ -21,6 +21,9 @@
  *         password:
  *           type: string
  *           description: The hashed password of the person
+ *         role:
+ *           type: string
+ *           description: Role of the user (e.g., admin, user)
  */
 
 /**
@@ -57,7 +60,17 @@
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '#/components/schemas/User'
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: The email address of the user
+ *               password:
+ *                 type: string
+ *                 description: The password of the user
  *     responses:
  *       200:
  *         description: Successfully logged in
@@ -73,6 +86,79 @@
  *         description: Missing fields
  *       401:
  *         description: Invalid credentials
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /admin/users:
+ *   get:
+ *     summary: Get all users
+ *     tags: [User]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /admin/user/{id}:
+ *   get:
+ *     summary: Get a user by ID
+ *     tags: [User]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: The ID of the user to retrieve
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: User found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /admin/user/{id}:
+ *   delete:
+ *     summary: Delete a user by ID
+ *     tags: [User]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: The ID of the user to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: User deleted successfully
+ *       404:
+ *         description: User not found
  *       500:
  *         description: Internal server error
  */
