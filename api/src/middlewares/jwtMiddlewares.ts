2b5e534ee0b66970d95d9828e5af0e09480df5a5
SarahOtmane <sarahotmane02@gmail.com>
2024-10-09 09:56:39 +0200

    verifyTokenAdmin

--- api/src/middlewares/jwtMiddlewares.ts
+++ api/src/middlewares/jwtMiddlewares.ts
@@ -7,6 +7,7 @@ dotenv.config();
 // Définir l'interface UserPayload
 interface UserPayload {
     id: number;
+    role: boolean;
 }
 
 // Étendre l'interface Express.Request pour ajouter `user`
@@ -14,6 +15,7 @@ declare global {
     namespace Express {
         interface Request {
             user?: UserPayload;
+            admin? : UserPayload
         }
     }
 }
@@ -34,6 +36,36 @@ export const verifyToken = async (req: Request, res: Response, next: NextFunctio
             });
 
             req.user = payload; 
+            next();
+        } else {
+            res.status(403).json({ message: "Accès interdit: token manquant" });
+        }
+    } catch (error) {
+        console.error(error); 
+        res.status(403).json({ message: "Accès interdit: token invalide" });
+    }
+};
+
+
+export const verifyTokenAdmin = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const token = req.headers['authorization'];
+
+        if (token) {
+            const payload = await new Promise<any>((resolve, reject) => {
+                jwt.verify(token, process.env.JWT_KEY as string, (error, decoded) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(decoded);
+                    }
+                });
+            });
+
+            req.admin = payload; 
+            if(payload.role){
+                res.status(403).json({ message: "Vous n'avez pas les autorisations pour effectuer cette opération !" });
+            }
             next();
         } else {
             res.status(403).json({ message: "Accès interdit: token manquant" });
