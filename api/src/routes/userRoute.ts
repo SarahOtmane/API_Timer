000af66b871a65ab7b7afc6d5844370b4da145fb
SarahOtmane <sarahotmane02@gmail.com>
2024-10-09 09:56:46 +0200

    config routes admin

--- api/src/routes/userRoute.ts
+++ api/src/routes/userRoute.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
-import * as userController from '../controllers/userController'; 
+import * as userController from '../controllers/userController';
+import * as jwtMiddlewares from '../middlewares/jwtMiddlewares'; 
 
 const router = Router();
 
 router.post('/register', userController.registerAUser);
 router.post('/login', userController.loginUser);
+
+// ROUTES ADMIN
+router.get('/admin/users', jwtMiddlewares.verifyTokenAdmin, userController.getAllUser);
+router.get('/admin/user/:id', jwtMiddlewares.verifyTokenAdmin, userController.getAllUser);
+router.delete('/admin/user/:id', jwtMiddlewares.verifyTokenAdmin, userController.deleteAUser);
 
 export default router;
